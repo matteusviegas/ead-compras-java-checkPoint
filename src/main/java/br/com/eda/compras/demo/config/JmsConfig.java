b7007eecdb7ecf0f6aa6c7979b40e2818e7f3967
ChallengeOne-MAT <SEU_EMAIL_DA_CONTA_DO_GITHUB>
2026-04-21 23:13:45 -0300

    feat: adicionando configuração JMS

---
+++ src/main/java/br/com/eda/compras/demo/config/JmsConfig.java
@@ -0,0 +1,10 @@
+package br.com.eda.compras.demo.config;
+
+public class JmsConfig {
+
+    public static final String FILA_COMPRA = "fila.compra";
+    public static final String TOPICO_COMPRA_PROCESSADA = "topico.compra.processada";
+
+    private JmsConfig() {
+    }
+}
